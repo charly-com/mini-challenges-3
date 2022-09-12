2789f84093fa627ddb6af1823f1dd6d78a3c13e5
charly-com <uchecharles223@gmail.com>
2022-09-12 21:59:48 +0100

    <" testing ">

--- src/binary-reversal/index.js
+++ src/binary-reversal/index.js
@@ -3,6 +3,26 @@
  *
  *  * @param {string} value
  */
-function binaryReversal(value) {}
+function binaryReversal(value) {
+    
+    // converting the value to binary
+    let binary = (parseInt(value).toString(2));
+    
+    // padding the binary with 0s to make it 8 digits
+    let padding = String(binary).padStart(8, "0");
+    
+    // reversing the binary
+    let splitedPad = padding.split("").reverse().join("");
+    
+    // converting the reversed binary to decimal
+    splitedPad = parseInt(splitedPad, 2) 
+    
+    return splitedPad.toString()
+
+  
+}
+
+console.log(binaryReversal("45"));
 
 module.exports = binaryReversal;
+ 
--- src/list-sorting/index.js
+++ src/list-sorting/index.js
@@ -1,3 +1,23 @@
-function listSorting(needle, haystack) {}
+function listSorting(needle, haystack) {
+    // The lastIndexOf() method returns the index (position) of the last occurrence of a specified value in a string.
+   if (typeof(haystack[0]) == "object") {
+    let result;
+    let row;
+    for (let i = 0; i < haystack.length; i++) {
+        for (let j = 0; j < haystack[i].length; j++) {
+            if (haystack[i][j] === needle) {
+                result = haystack[i].lastIndexOf(needle);
+                row = i;
+            }
+        }
+     }
+     if (row == undefined) return -1;
+     return [row, result]
+
+   } 
+   else {
+    return haystack.lastIndexOf(needle);
+   }
+}
 
 module.exports = listSorting;
